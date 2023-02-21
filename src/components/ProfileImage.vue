2956b885c1aec9b3e421a5344f2adebc9f6924ee
Alkis Tzaras <at@saper-vedere.eu>
2023-02-21 13:43:46 +0100

    Configures the button to be dynamic

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <img
+    :src="imageLink"
+    class="h-8 w-8 rounded-3xl object-contain"
+    alt="User profile image"
+  />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink:
+        "https://pbs.twimg.com/profile_images/1371713662990303233/-kbnQGFp_400x400.jpg",
+    };
+  },
+};
+</script>
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,11 +1,48 @@
 import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
 
 import MainNav from "@/components/MainNav.vue";
 
 describe("MainNav", () => {
   it("displays company name", () => {
     render(MainNav);
-    const companyName = screen.getByText("Alkis careerss");
+    const companyName = screen.getByText("Alkis careers");
     expect(companyName).toBeInTheDocument();
+  });
+
+  it("displays menu items for navigation", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuTexts = navigationMenuItems.map(
+      (item) => item.textContent
+    );
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Alkis Corp",
+      "Students",
+      "Jobs",
+    ]);
+  });
+
+  describe("When the user logs in", () => {
+    it("displays user profile picture", async () => {
+      render(MainNav);
+
+      let profileImage = screen.queryByRole("img", {
+        name: /user profile image/i,
+      });
+      expect(profileImage).not.toBeInTheDocument();
+
+      const loginButton = screen.getByRole("button", {
+        name: /sign in/i,
+      });
+      await userEvent.click(loginButton);
+
+      profileImage = screen.getByRole("img", {
+        name: /user profile image/i,
+      });
+      expect(profileImage).toBeInTheDocument();
+    });
   });
 });
