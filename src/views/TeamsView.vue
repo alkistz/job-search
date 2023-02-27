a197b37c87a0bdabd443a8c9b115c6f53840c525
Alkis Tzaras <at@saper-vedere.eu>
2023-02-27 15:41:41 +0100

    Did some more slot stuff

---
+++ src/views/TeamsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <HeaderContainer>
+    <template #title>
+      <h1 class="w-full text-4xl font-normal">Teams</h1>
+    </template>
+    <template #subtitle>
+      <h2 class="my-4 w-full text-base font-light">
+        It is awesome to work here
+      </h2>
+    </template>
+  </HeaderContainer>
+</template>
+
+<script>
+import HeaderContainer from "@/components/shared/HeaderContainer.vue";
+
+export default {
+  name: "TeamsView",
+  components: {
+    HeaderContainer,
+  },
+};
+</script>
