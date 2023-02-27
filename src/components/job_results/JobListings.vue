1050b258e4959c111aff9897bd15464b8a5dca98
Alkis Tzaras <at@saper-vedere.eu>
2023-02-27 10:35:13 +0100

    added pinia to tak ecare of the jobs and user

--- src/components/job_results/JobListings.vue
+++ src/components/job_results/JobListings.vue
@@ -1,8 +1,9 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <JobListing v-for="job in displayedJobs" :key="job.id" :job="job" />
+      <job-listing v-for="job in displayedJobs" :key="job.id" :job="job" />
     </ol>
+
     <div class="mx-auto mt-8">
       <div class="flex flex-row flex-nowrap">
         <p class="flex-grow text-sm">Page {{ currentPage }}</p>
@@ -10,17 +11,21 @@
         <div class="flex items-center justify-center">
           <router-link
             v-if="previousPage"
+            role="link"
             :to="{ name: 'JobResults', query: { page: previousPage } }"
             class="mx-3 text-sm font-semibold text-brand-blue-1"
-            >Previous</router-link
           >
+            Previous
+          </router-link>
 
           <router-link
             v-if="nextPage"
+            role="link"
             :to="{ name: 'JobResults', query: { page: nextPage } }"
             class="mx-3 text-sm font-semibold text-brand-blue-1"
-            >Next</router-link
           >
+            Next
+          </router-link>
         </div>
       </div>
     </div>
@@ -28,19 +33,14 @@
 </template>
 
 <script>
-import axios from "axios";
+import { mapActions, mapState } from "pinia";
+
 import JobListing from "@/components/job_results/JobListing.vue";
+import { useJobsStore, FETCH_JOBS } from "@/stores/jobs";
 
 export default {
   name: "JobListings",
-  components: {
-    JobListing,
-  },
-  data() {
-    return {
-      jobs: [],
-    };
-  },
+  components: { JobListing },
   computed: {
     currentPage() {
       return Number.parseInt(this.$route.query.page || "1");
@@ -50,21 +50,26 @@ export default {
       const firstPage = 1;
       return previousPage >= firstPage ? previousPage : undefined;
     },
-    nextPage() {
-      const nextPage = this.currentPage + 1;
-      const maxPage = this.jobs.length / 10;
-      return nextPage <= maxPage ? nextPage : undefined;
-    },
-    displayedJobs() {
-      const pageNumber = this.currentPage;
-      const firstJobIndex = (pageNumber - 1) * 10;
-      const lastJobIndex = pageNumber * 10;
-      return this.jobs.slice(firstJobIndex, lastJobIndex);
-    },
+    ...mapState(useJobsStore, {
+      jobs: "jobs",
+      nextPage() {
+        const nextPage = this.currentPage + 1;
+        const maxPage = Math.ceil(this.jobs.length / 10);
+        return nextPage <= maxPage ? nextPage : undefined;
+      },
+      displayedJobs() {
+        const pageNumber = this.currentPage;
+        const firstJobIndex = (pageNumber - 1) * 10;
+        const lastJobIndex = pageNumber * 10;
+        return this.jobs.slice(firstJobIndex, lastJobIndex);
+      },
+    }),
   },
   async mounted() {
-    const response = await axios.get("http://localhost:3000/jobs");
-    this.jobs = response.data;
+    this.FETCH_JOBS();
+  },
+  methods: {
+    ...mapActions(useJobsStore, [FETCH_JOBS]),
   },
 };
 </script>
