4c9cc34889a361a8826171612355a12b40b3417b
Alkis Tzaras <at@saper-vedere.eu>
2023-02-27 14:19:28 +0100

    added slots

--- src/components/job_results/job_filter_sidebar/JobFilterSidebar.vue
+++ src/components/job_results/job_filter_sidebar/JobFilterSidebar.vue
@@ -2,12 +2,35 @@
   <div
     class="flex w-96 flex-col border-r border-solid border-brand-gray-1 bg-white p-4"
   >
-    Job Filter sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <ActionButton text="Clear Filters" type="secondary" />
+        </div>
+      </div>
+      <CollapsibleAccordion header="Degree">
+        <p>placehodler for now</p>
+      </CollapsibleAccordion>
+      <CollapsibleAccordion header="Job types">
+        <p>placehodler for now</p>
+      </CollapsibleAccordion>
+      <JobFiltersSidebarOrganizations />
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/shared/ActionButton.vue";
+import CollapsibleAccordion from "@/components/shared/CollapsibleAccordion.vue";
+import JobFiltersSidebarOrganizations from "@/components/job_results/job_filter_sidebar/JobFiltersSidebarOrganizations.vue";
+
 export default {
   name: "JobFilterSidebar",
+  components: {
+    ActionButton,
+    CollapsibleAccordion,
+    JobFiltersSidebarOrganizations,
+  },
 };
 </script>
